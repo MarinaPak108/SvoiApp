076d693390d9d04f9421b3f01d2b1bcab6ccdabb
pakmarina108@gmail.com <pm@mail.ru>
2024-06-09 16:25:26 +0300

    added try/catch. saves exception msg to logger and redirects to home page

--- src/main/java/com/svoiapp/controller/web/MainController.java
+++ src/main/java/com/svoiapp/controller/web/MainController.java
@@ -25,6 +25,8 @@ import org.springframework.stereotype.Controller;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 
 @Controller
@@ -32,106 +34,148 @@ import java.util.HashMap;
 public class MainController {
     private final UserService service;
     private final DocService dService;
-    private final MailService mailService;
+    private static final Logger logger = Logger.getLogger(MainController.class.getName());
 
-    public MainController(UserService service, DocService dService, MailService mailService) {
+    public MainController(UserService service, DocService dService) {
         this.service = service;
         this.dService = dService;
-        this.mailService = mailService;
     }
 
     @GetMapping("/home")
     public String home (Model model) throws MessagingException {
-        Authentication authentication =
-                SecurityContextHolder.getContext().getAuthentication();
-        String[] loginEmail = authentication.getName().split("/__/");
-        String login = loginEmail[0];
-        if(login.equals("anonymousUser")){
-            model.addAttribute("user", "Welcome to SVOI app");
-        }
-        else {
-            DataEntity data = service.getData(login);
-            model.addAttribute("user", "Hi "+ login + ", welcome to SVOI app");
-            if(data.getPin() != null && data.getConfirmed()==false){
-                model.addAttribute("formData", new CreatePinFromData());
-                model.addAttribute("msg", "Код для верификации был выслан на Ваш почтовый ящик. Также рекомендуем проверить папку 'СПАМ'.");
+        try{
+            Authentication authentication =
+                    SecurityContextHolder.getContext().getAuthentication();
+            String[] loginEmail = authentication.getName().split("/__/");
+            String login = loginEmail[0];
+            if(login.equals("anonymousUser")){
+                model.addAttribute("user", "Welcome to SVOI app");
+
             }
+            else {
+                DataEntity data = service.getData(login);
+                model.addAttribute("user", "Hi "+ login + ", welcome to SVOI app");
+                if(data.getPin() != null && data.getConfirmed()==false){
+                    model.addAttribute("formData", new CreatePinFromData());
+                    model.addAttribute("msg", "Код для верификации был выслан на Ваш почтовый ящик. Также рекомендуем проверить папку 'СПАМ'.");
+                }
+            }
+            return "home";
         }
-        return "home";
+        catch (Exception e){
+            String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+            return catchExceptionToLogger(e.getMessage(),methodName);
+        }
     }
 
     @PostMapping("/home")
     public String postHome (@ModelAttribute("formData") CreatePinFromData formData){
-        Authentication authentication =
-                SecurityContextHolder.getContext().getAuthentication();
-        String[] loginEmail = authentication.getName().split("/__/");
-        String login = loginEmail[0];
-        String pin = formData.toOnePin();
-        service.checkPin(login, pin);
-        return "home";
+        try{
+            Authentication authentication =
+                    SecurityContextHolder.getContext().getAuthentication();
+            String[] loginEmail = authentication.getName().split("/__/");
+            String login = loginEmail[0];
+            String pin = formData.toOnePin();
+            service.checkPin(login, pin);
+            return "home";
+        }
+        catch (Exception e){
+            String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+            return catchExceptionToLogger(e.getMessage(),methodName);
+        }
     }
 
     @RequestMapping("/service")
     public String service(Model model) throws MessagingException {
-        String[] loginEmail = CustomAuthHanlder.getLogin();
-        String login = loginEmail[0];
-        String email = loginEmail[1];
-        boolean isEmailConfirmed = service.isEmailAuthorised(login, email);
-        if(isEmailConfirmed){
-            model.addAttribute("isChecked", true);
+        try{
+            String[] loginEmail = CustomAuthHanlder.getLogin();
+            String login = loginEmail[0];
+            String email = loginEmail[1];
+            boolean isEmailConfirmed = service.isEmailAuthorised(login, email);
+            if(isEmailConfirmed){
+                model.addAttribute("isChecked", true);
+            }
+            model.addAttribute("userName", login);
+            return"service";
+        }catch (Exception e) {
+            String methodName = new Object() {
+            }.getClass().getEnclosingMethod().getName();
+            return catchExceptionToLogger(e.getMessage(), methodName);
         }
-        model.addAttribute("userName", login);
-        return"service";
     }
 
     @GetMapping("/visaExt")
     public String visaExtend (Model model){
-        model.addAttribute("formData", new CreateVisaExtendFormData());
-        return "visa";
+        try{
+            model.addAttribute("formData", new CreateVisaExtendFormData());
+            return "visa";
+        }catch (Exception e) {
+            String methodName = new Object() {
+            }.getClass().getEnclosingMethod().getName();
+            return catchExceptionToLogger(e.getMessage(), methodName);
+        }
     }
 
     @PostMapping("/visaExt")
     public String processVisa (Model model, @ModelAttribute("formData") CreateVisaExtendFormData formData,
                                HttpServletRequest request
-    ) throws IOException{
-        Authentication authentication =
-                SecurityContextHolder.getContext().getAuthentication();
-        String[] loginEmail = authentication.getName().split("/__/");
-        String login = loginEmail[0];
-        String visaType = request.getParameter("visatype");
-        Boolean isSchool = Boolean.parseBoolean(request.getParameter("isSchool"));
-        Boolean isWork = Boolean.parseBoolean(request.getParameter("isWork"));
-        HashMap<String, String> data = dService.prepareEntity(formData, visaType);
-        data = dService.fillSchoolWork(data,formData, visaType, isSchool, isWork);
-        Boolean isSaved = dService.replaceText(data, visaType, login);
-        if (isSaved){
-            return "download";
-        }
-        else{
-            model.addAttribute("formData", new CreateVisaExtendFormData());
-            model.addAttribute("msg", "Что то пошло не так. Попробуйте еще раз");
-            return "visa";
+    ){
+        try{
+            Authentication authentication =
+                    SecurityContextHolder.getContext().getAuthentication();
+            String[] loginEmail = authentication.getName().split("/__/");
+            String login = loginEmail[0];
+            String visaType = request.getParameter("visatype");
+            Boolean isSchool = Boolean.parseBoolean(request.getParameter("isSchool"));
+            Boolean isWork = Boolean.parseBoolean(request.getParameter("isWork"));
+            HashMap<String, String> data = dService.prepareEntity(formData, visaType);
+            data = dService.fillSchoolWork(data,formData, visaType, isSchool, isWork);
+            Boolean isSaved = dService.replaceText(data, visaType, login);
+            if (isSaved){
+                return "download";
+            }
+            else{
+                model.addAttribute("formData", new CreateVisaExtendFormData());
+                model.addAttribute("msg", "Что то пошло не так. Попробуйте еще раз");
+                return "visa";
+            }
+        }catch (Exception e) {
+            String methodName = new Object() {
+            }.getClass().getEnclosingMethod().getName();
+            return catchExceptionToLogger(e.getMessage(), methodName);
         }
     }
 
     @GetMapping("/download")
     public ResponseEntity<Resource> downloadFile(Model model) {
-        Authentication authentication =
-                SecurityContextHolder.getContext().getAuthentication();
-        String[] loginEmail = authentication.getName().split("/__/");
-        String login = loginEmail[0];
-        model.addAttribute("msg", "Нажмите кнопку 'Скачать' чтобы загрузить документ.");
-        // Get latest file name
-        String fileName = dService.getLatestFile("/Services/Visa/"+login+"/");
-        // Load the file as a Resource
-        Resource resource = dService.loadFileAsResource(fileName, "/Services/Visa/"+login+"/");
+        try{
+            Authentication authentication =
+                    SecurityContextHolder.getContext().getAuthentication();
+            String[] loginEmail = authentication.getName().split("/__/");
+            String login = loginEmail[0];
+            model.addAttribute("msg", "Нажмите кнопку 'Скачать' чтобы загрузить документ.");
+            // Get latest file name
+            String fileName = dService.getLatestFile("/Services/Visa/"+login+"/");
+            // Load the file as a Resource
+            Resource resource = dService.loadFileAsResource(fileName, "/Services/Visa/"+login+"/");
 
-        // Add the resource to the model
-        model.addAttribute("resource", resource);
+            // Add the resource to the model
+            model.addAttribute("resource", resource);
 
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(resource);//
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(resource);//
+        }catch (Exception e) {
+            String methodName = new Object() {
+            }.getClass().getEnclosingMethod().getName();
+            logger.info("******************* in mehtod: "+methodName+". "+e.getMessage());
+            return (ResponseEntity<Resource>) ResponseEntity.EMPTY;
+        }
+    }
+
+    static String catchExceptionToLogger (String msg, String methodName) {
+        logger.info("******************* in mehtod: "+methodName+". "+msg);
+        return "home";
     }
 }
